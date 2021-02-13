be6b8528db2d1a09b18eab0ad6bf7a5b4f08aaa1
Pranav Reddy Charkupalli <pranavcharku07@gmail.com>
2021-02-13 02:49:47 -0600

    Account Functionality
    Created a home page, create account and login pages, and made a user datab ase.

--- flaskapp.py
+++ flaskapp.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.secret_key = "treehacks"
 app.permanent_session_lifetime = timedelta(days=3)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_BINDS'] = {'requests' : 'sqlite:///requests.sqlite3',
+                                    'pros': 'sqlite:///pros.sqlite3'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -14,52 +16,117 @@ db = SQLAlchemy(app)
 class users(db.Model):
     user = db.Column(db.String(100), primary_key=True)
     pwd = db.Column(db.String(100))
-#     mbti = db.Column(db.String(6))
-#     dnd = db.Column(db.String(15))
-#     types = db.Column(db.String(10))
+    first = db.Column(db.String(100))
+    last = db.Column(db.String(100))
+    age = db.Column(db.Integer())
+    gender = db.Column(db.String(1))
+    latt = db.Column(db.Numeric(10,7))
+    long = db.Column(db.Numeric(10,7))
 
-    def __init__(self, user, pwd):
+    def __init__(self, user, pwd, first, last, gender, age):
         self.user = user
         self.pwd = pwd
-#         self.mbti = "?"
-#         self.dnd = "?"
-#         self.types = "?"
+        self.first = first
+        self.last = last
+        self.age = age
+        self.gender = gender
+        self.latt = -360
+        self.long = -360
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    # if "user" in session:
-    #     flash("You are already logged in")
-    #     return redirect(url_for("profile", name=session["user"]))
-    
-    if request.method == 'POST':
-        session.permanent = True
-        user = (request.form["username"]).strip()
-        password = request.form["password"].strip()
-        if(user == "" or password == ""):
-            flash("Please fill out both fields")
+    if "user" in session:
+        if request.method == 'POST':
+            session.clear()
+            flash("You have logged out")
             return redirect(url_for("main"))
-        if(user.find(" ") != -1):
-            flash("No whitespace allowed in username")
-            return redirect(url_for("main"))
+        foundUser = users.query.filter_by(user=session["user"].lower()).first()
+        if not(foundUser):
+            session.clear()
+            flash("An error has occurred. Please login again")
+            return redirect(url_for("login"))
+        arr = [foundUser.first, foundUser.last]
+        return render_template("home.html", arr = arr)
 
+    else:
+        if request.method == 'POST':
+            if request.form.get("login"):
+                return redirect(url_for("login"))
+            else:
+                return redirect(url_for("account"))
+        return render_template("anon_home.html")
+
+@app.route('/create-account', methods=['GET', 'POST'])
+def account():
+    if "user" in session:
+        flash("You are already logged in")
+        return redirect(url_for("main"))
+
+    if request.method == 'POST':
+        user = request.form["username"].strip()
+        pwd = request.form["pwd"].strip()
+        age = request.form["age"].strip()
+        first = request.form["first"].strip()
+        last = request.form["last"].strip()
+        gender = request.form["gender"]
+
+        if(user == "" or pwd == "" or age == "" or first == "" or last == ""):
+            flash("Please fill out all fields")
+            return redirect(url_for("account"))
+        elif(user.find(" ") != -1):
+            flash("No whitespace allowed in username")
+            return redirect(url_for("account"))
+        elif not(age.isnumeric()):
+            flash("Please make age a number")
+            return redirect(url_for("account"))
+        
+        age = int(age)
         foundUser = users.query.filter_by(user=user.lower()).first()
         if not(foundUser):
-            usr = users(user.lower(), password)
+            usr = users(user.lower(), pwd, first, last, gender, age)
             db.session.add(usr)
             db.session.commit()
         else:
-            if(foundUser.pwd != password):
-                flash("Incorrect password")
-                return render_template("login.html")
-        
+            flash("Username is already in use, please choose another one")
+            return redirect(url_for("account"))
+
+        session.permanent = True
+        session["user"] = user.lower()
+        return redirect(url_for("main"))
+
+    return render_template("account.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if "user" in session:
+        flash("You are already logged in")
+        return redirect(url_for("main"))
+
+    if request.method == 'POST':
+        user = (request.form["username"]).strip()
+        pwd = request.form["pwd"].strip()
+        if(user == "" or pwd == ""):
+            flash("Please fill out both fields")
+            return redirect(url_for("login"))
+        if(user.find(" ") != -1):
+            flash("No whitespace allowed in username")
+            return redirect(url_for("login"))
+
         foundUser = users.query.filter_by(user=user.lower()).first()
-
-        # session["user"] = user.lower()
-
-        flash("You've logged in, "+ user)
+        if not(foundUser):
+            flash("Username doesn't exist")
+            return redirect(url_for("login"))
+        else:
+            if(foundUser.pwd != pwd):
+                flash("Incorrect password")
+                return redirect(url_for("login"))
+        
+        session.permanent = True
+        session["user"] = user.lower()
         return redirect(url_for("main"))
 
     return render_template("login.html")
+
 
 if __name__ == "__main__":
     db.create_all()
